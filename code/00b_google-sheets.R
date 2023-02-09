aa48397c00fe55a57329cd2768a0526bc9388ca9
an-bui <a.thanh.bui@gmail.com>
2023-02-09 11:29:25 -0800

    new surface area and perimeter 2023-02-09

--- code/00b_google-sheets.R
+++ code/00b_google-sheets.R
@@ -74,7 +74,7 @@ sheet_write_csv(weight_sheet)
 
 sa_peri_sheet <- read_sheet(sheet_id, sheet = "05a-scans") 
 
-# last updated: 2023-02-02
+# last updated: 2023-02-09
 sheet_write_csv(sa_peri_sheet)
 
 # ⊣ h. branching order (still need to clean up) -------------
