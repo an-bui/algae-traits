6dc38ade0a0cd7d11d2f738f72e96ec0f62799c7
an-bui <a.thanh.bui@gmail.com>
2024-01-24 21:35:04 -0800

    new scans for 2024-01-24, figures etc.

--- code/00c_trait-data.R
+++ code/00c_trait-data.R
@@ -76,7 +76,7 @@ weight <- read_csv(here::here("data", "google-sheet-traits", "weight_sheet_2024-
 
 # ⊣ g. surface area and perimeter ---------------------------
 
-sa_peri <- read_csv(here::here("data", "google-sheet-traits", "sa_peri_sheet_2024-01-23.csv"))
+sa_peri <- read_csv(here::here("data", "google-sheet-traits", "sa_peri_sheet_2024-01-24.csv"))
 
 # ⊣ h. branching order (still need to clean up) -------------
 
