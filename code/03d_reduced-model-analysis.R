1709d2da0c90d1f3f887afc0422a7e96399f0209
an-bui <a.thanh.bui@gmail.com>
2025-03-25 13:34:28 -0700

    new figures for reduced models

--- code/03d_reduced-model-analysis.R
+++ code/03d_reduced-model-analysis.R
@@ -16,38 +16,38 @@ keep_4trait_combination <- combo_4traits %>%
   mutate(combo_number = rownames(.)) %>% 
   # count TRUE and FALSE occurrences
   mutate(count = map(
-    pairwise_padj_significant_euc,
+    pairwise_significant_euc,
     ~ .x %>% unlist() %>% table() 
   )) %>% 
   # number of insignificant pairwise comparisons
   mutate(count_false = pmap(
-    list(x = pairwise_padj_conserved_euc, y = count),
+    list(x = pairwise_conserved_euc, y = count),
     function(x, y) case_when(
       x == "no" ~ y[[1]],
       x == "yes" ~ 0
     )
   )) %>% 
-  filter(count_false %in% c(0, 1)) %>% 
+  filter(count_false %in% c(0)) %>% 
   # filter(pairwise_padj_conserved_euc == "yes") %>% 
   unite(trait1:trait4, col = trait_list, sep=", ") %>% 
   mutate(number_of_traits = 4)
 
-keep_3trait_combination <- combo_3traits  %>% 
+keep_3trait_combination <- combo_3traits %>% 
   mutate(combo_number = rownames(.)) %>% 
   # count TRUE and FALSE occurrences
   mutate(count = map(
-    pairwise_padj_significant_euc,
+    pairwise_significant_euc,
     ~ .x %>% unlist() %>% table() 
   )) %>% 
   # number of insignificant pairwise comparisons
   mutate(count_false = pmap(
-    list(x = pairwise_padj_conserved_euc, y = count),
+    list(x = pairwise_conserved_euc, y = count),
     function(x, y) case_when(
       x == "no" ~ y[[1]],
       x == "yes" ~ 0
     )
   )) %>% 
-  filter(count_false %in% c(0, 1)) %>% 
+  filter(count_false %in% c(0)) %>% 
   filter(!(combo_number == 3)) %>% 
   unite(trait1:trait3, col = trait_list, sep=", ") %>% 
   mutate(number_of_traits = 3)
@@ -227,20 +227,23 @@ reduced_combinations <- bind_rows(
 # 3 traits combo 7: row 69
 # 3 traits combo 22: row 73
 # 4 traits combo 15: row 8
-combo_4traits_biplots <- reduced_combinations[[22]][[8]]
-combo_3traits_combo7_biplots <- reduced_combinations[[22]][[69]]
-combo_3traits_combo22_biplots <- reduced_combinations[[22]][[73]]
+combo_4traits_combo2_biplots <- reduced_combinations[[22]][[1]]
+combo_4traits_combo5_biplots <- reduced_combinations[[22]][[2]]
+combo_3traits_combo7_biplots <- reduced_combinations[[22]][[3]]
+combo_3traits_combo9_biplots <- reduced_combinations[[22]][[4]]
 
 combo_biplots_together <- list(
-  combo_4traits_biplots,
+  combo_4traits_combo2_biplots,
+  combo_4traits_combo5_biplots,
   combo_3traits_combo7_biplots,
-  combo_3traits_combo22_biplots
+  combo_3traits_combo9_biplots
 )
 
 combo_biplot_file_names <- list(
-  "biplot-4traits",
+  "biplot-4traits-combo2",
+  "biplot-4traits-combo5",
   "biplot-3traits-combo7",
-  "biplot-3traits-combo22"
+  "biplot-3traits-combo9"
 )
 
 for(i in 1:length(combo_biplots_together)) {
